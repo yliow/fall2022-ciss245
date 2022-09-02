f63654a1956be545103b58f516e456832db32ef4
Yihsiang Liow <yliow@ccis.edu>
2022-09-02 16:22:53 -0400

    no msg

--- 2022-09-02/main.cpp
+++ 2022-09-02/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <cmath>
 
+int f(int x)
+{
+    std::cout << "x of f = " << x << '\n';
+    x = 0;
+    std::cout << "x of f = " << x << '\n';
+    return 42;
+}
+int g(int x)
+{
+    std::cout << "x of g = " << x << '\n';
+    x = 0;
+    std::cout << "x of g = " << x << '\n';
+    return 42;
+}
+
 bool isprime(int n)
 {
     if (n < 2)
@@ -22,9 +37,18 @@ bool isprime(int n)
     
 int main()
 {
-    for (int n = 1000000000; n < 2000000000; ++n)
-    {
-        std::cout << n << ' ' << isprime(n) << '\n';
-    }
+    // for (int n = 1000000000; n < 2000000000; ++n)
+    // {
+    //     std::cout << n << ' ' << isprime(n) << '\n';
+    // }
+    int x = 1;
+    std::cout << "x of main = " << x << '\n';
+    f(x);
+    std::cout << "x of main = " << x << '\n';
+
+    std::cout << "x of main = " << x << '\n';
+    g(x);
+    std::cout << "x of main = " << x << '\n';
+
     return 0;
 }
