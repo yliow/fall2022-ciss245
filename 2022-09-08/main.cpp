a6b92058ff1c66afd420f6f34149ac125f7565a1
Yihsiang Liow <yliow@ccis.edu>
2022-09-08 16:00:02 -0400

    no msg

--- 2022-09-08/main.cpp
+++ 2022-09-08/main.cpp
@@ -24,13 +24,53 @@ void println(int x[], int n)
     std::cout << '\n';
 }
 
+int sum(int x[], int n)
+{
+    int s = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        s += x[i];
+    }
+    return s;
+}
+
+//double avg(int x[], int n);
+
+int max(int x[], int n)
+{
+    int m = x[0];
+    for (int i = 1; i < n; ++i)
+    {
+        if (x[i] > m)
+        {
+            m = x[i];
+        }
+    }
+    return m;
+}
+
+int linearsearch(int x[], int n, int target)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        if (x[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     srand((unsigned int) time(NULL));
     
     const int N = 1024;
     int x[N];
-    randarray(x, 10);
-    println(x, 10);
+    randarray(x, 5);
+    println(x, 5);
+    int s = sum(x, 5);
+    std::cout << s << '\n';
+    std::cout << max(x, 5) << '\n';
     return 0;
 }
