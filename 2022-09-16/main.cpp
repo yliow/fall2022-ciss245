8a76ec058d6384cf679af56db1a002013043410a
Yihsiang Liow <yliow@ccis.edu>
2022-09-16 16:05:53 -0400

    no msg

--- 2022-09-16/main.cpp
+++ 2022-09-16/main.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 
+void Array_print(int x[], int size=10);
 
-void Array_print(int x[])
+void Array_print(int x[], int size)
 {
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < size; ++i)
     {
         std::cout << x[i] << ' ';
     }
 }
 
 
-void Array_println(int x[])
+void Array_println(int x[], int size=10)
 {
-    Array_print(x);
+    Array_print(x, size);
     std::cout << '\n';
+}
+
+int sum(int i, int j)
+{
+    return i + j;
 }
 
 int main()
 {
-    int x[10];
-    std::cout << "x: ";
-    Array_println(x);
-    
+    // int x[1024];
+    // std::cout << "x: ";
+    // Array_println(x); // <-- warning
+    // Array_println(x, 15);
+    // Array_println(x, 3);
+
+    int i = 0, j = 1;
+    int z = sum(i, j);
+    std::cout << z << '\n';
     return 0;
 }
