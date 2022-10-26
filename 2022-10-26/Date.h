48b7244091c6a94708867dec853cb7e3379b4b1b
Yihsiang Liow <yliow@ccis.edu>
2022-10-26 15:53:50 -0400

    no msg

---
+++ 2022-10-26/Date.h
@@ -0,0 +1,41 @@
+// Date.h
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+
+class Date
+{
+public:
+    Date();
+    Date(int yyyy, int mm, int dd);
+    Date(const Date &);
+    ~Date();
+    void init(int, int, int);
+    void print() const;
+    void add_y(int);
+    void add_m(int);
+    void add_d(int);
+    void add_m_d(int, int);
+    void add_date(const Date &);
+    int get_year() const;
+    int year() const;
+    int & year();
+    void set_year(int);
+    int get_month() const;
+    void set_month(int);
+    int get_day() const;
+    void set_day(int);
+    bool equals(const Date &) const;
+    bool operator==(const Date &) const;
+    bool operator!=(const Date &) const;
+    const Date & operator+=(int);
+    Date operator+(int) const;
+private:
+    int yyyy_, mm_, dd_;
+};
+
+std::ostream & operator<<(std::ostream &, const Date &);
+Date operator+(int, const Date &);
+
+#endif
