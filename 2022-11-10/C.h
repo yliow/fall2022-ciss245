7d856422397cda76373157f7bc369ec9c6d9918f
Yihsiang Liow <yliow@ccis.edu>
2022-11-10 15:12:01 -0500

    no msg

---
+++ 2022-11-10/C.h
@@ -0,0 +1,12 @@
+#ifndef C_H
+#define C_H
+
+class C
+{
+public:
+    C(int x): i(x) {}
+    int i;
+    static int j;
+};
+
+#endif
---
+++ 2022-11-10/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "C.h"
+
+int main()
+{
+    C c(42);
+    return 0;
+}
