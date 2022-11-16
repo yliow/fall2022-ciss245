65a9b0c4ad9965f6d3ae2b7494be4540dd84e45e
Yihsiang Liow <yliow@ccis.edu>
2022-11-16 16:29:00 -0500

    no msg

--- 2022-11-16/snake/main.cpp
+++ 2022-11-16/snake/main.cpp
@@ -3,6 +3,24 @@
 #include <ctime>
 #include <vector>
 
+const int N = 0;
+const int S = 1;
+const int E = 2;
+const int W = 3;
+const int NO_OPTION = 4;
+
+int to_direction(char option)
+{
+    switch (option)
+    {
+        case 'w': return N;
+        case 's': return S;
+        case 'd': return E;
+        case 'a': return W;            
+    }
+    return NO_OPTION;
+}
+
 class Surface
 {
 public:
@@ -82,7 +100,42 @@ public:
     int y() const { return y_; }
     void draw()
     {
-        (*psurface_)(x_, y_) = 'H';
+        char h[4] = {'^', 'V', '>', '<'};
+        (*psurface_)(x_, y_) = h[dir_];
+    }
+    void move(int direction)
+    {
+        switch (direction)
+        {
+            case N:
+            {
+                --y_;
+                if (y_ < 0) y_ = 0;
+                dir_ = N;
+                break;
+            }
+            case S:
+            {
+                ++y_;
+                if (y_ > 4) y_ = 4;
+                dir_ = S;
+                break;
+            }
+            case E:
+            {
+                ++x_;
+                if (x_ > 9) x_ = 9;
+                dir_ = E;
+                break;
+            }
+            case W:
+            {
+                --x_;
+                if (x_ < 0) x_ = 0;
+                dir_ = W;
+                break;
+            }
+        }
     }
 private:
     Surface * psurface_;
@@ -98,18 +151,20 @@ int main()
     Surface surface;
     Apple a(&surface, 0, 0);
     //Snake snake;
-    Head head(&surface, 4, 3, 1);
+    Head head(&surface, 2, 3, 1);
 
     bool game_ended = false;
     while (not game_ended)
     {
         surface.clear();
         a.draw();
-        //surface.put(head);
+        head.draw();
         surface.draw();
         std::cout << "w-north s-south d-east a-west n-no input: ";
         char option;
         std::cin >> option;
+        int direction = to_direction(option);
+        head.move(direction);
     }
     
     return 0;
